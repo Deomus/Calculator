61091bfcf391e4bc4cfc4b99f3dedd51206d11d7
Deomus <78692681+Deomus@users.noreply.github.com>
2023-04-30 15:33:16 +0300

    creating token method

--- main.cpp
+++ main.cpp
@@ -1,38 +1,144 @@
+//3.10.–азработка калькул€тора.ѕрограмма должна
+//обеспечивать ввод пользователем с клавиатуры строки символов,
+//заканчивающейс€ символом Ђ¬озврат кареткиї(0x0D в шестнадцатиричной форме),
+//далее осуществл€ть синтаксический разбор полученной строки,
+//выдел€€ из нее такие операции, как УsinФ,
+//УcosФ, УtgФ, УlnФ, УsqrtФ(квадратный корень), У ^Ф(возведение в степень),
+//У *Ф, У / Ф, У + Ф, У - Ф, У(Ф, У)Ф и операнды(числа участвующие в операци€х).
+//ƒалее, в соответствии с полученным выражением,
+//программе необходимо реализовать математические действи€ и
+//вывести на экран результат
 #include <iostream>
 #include <string>
+#include <stack>
+#include <cmath>
+#include <map>
+
 using namespace std;
 
-void fine(string raw)
+int precedence(char op)
 {
-	if (raw == "+")
-	{
-	}
-	else if (raw == "-")
-	{
-	}
-	else if (raw == "/")
-	{
-	}
-	else if (raw == "*")
-	{
-	}
-	else if (raw == "")
-	{
+	if (op == '+' || op == '-')
+		return 1;
+	if (op == '*' || op == '/')
+		return 2;
+	if (op == '^')
+		return 3;
+	return 0;
+}
 
+double applyOp(double a, double b, char op)
+{
+	switch (op)
+	{
+	case '+':
+		return a + b;
+	case '-':
+		return a - b;
+	case '*':
+		return a * b;
+	case '/':
+		return a / b;
+	case '^':
+		return pow(a, b);
 	}
+}
+
+double evaluate(string tokens)
+{
+	stack<double> values;
+	stack<char> ops;
+
+	for (int i = 0; i < tokens.length(); i++)
+	{
+		if (tokens[i] == ' ')
+			continue;
+
+		else if (tokens[i] == '(')
+		{
+			ops.push(tokens[i]);
+		}
+
+		else if (isdigit(tokens[i]))
+		{
+			double val = 0;
+
+			while (i < tokens.length() && isdigit(tokens[i]))
+			{
+				val = (val * 10) + (tokens[i] - '0');
+				i++;
+			}
+
+			values.push(val);
+
+			i--;
+		}
+
+		else if (tokens[i] == ')')
+		{
+			while (!ops.empty() && ops.top() != '(')
+			{
+				double val2 = values.top();
+				values.pop();
+
+				double val1 = values.top();
+				values.pop();
+
+				char op = ops.top();
+				ops.pop();
+
+				values.push(applyOp(val1, val2, op));
+			}
+
+			if (!ops.empty())
+				ops.pop();
+		}
+
+		else
+		{
+			while (!ops.empty() && precedence(ops.top()) >= precedence(tokens[i]))
+			{
+				double val2 = values.top();
+				values.pop();
+
+				double val1 = values.top();
+				values.pop();
+
+				char op = ops.top();
+				ops.pop();
+
+				values.push(applyOp(val1, val2, op));
+			}
+
+			ops.push(tokens[i]);
+		}
+	}
+
+	while (!ops.empty())
+	{
+		double val2 = values.top();
+		values.pop();
+
+		double val1 = values.top();
+		values.pop();
+
+		char op = ops.top();
+		ops.pop();
+
+		values.push(applyOp(val1, val2, op));
+	}
+
+	return values.top();
 }
 
 int main()
 {
-	string entered;
-	cin >> entered;
-
-	string current;
-	for (int i = 0; i < entered.size(); i++)
+	while (1)
 	{
-		if (entered[i] == ' ')
-			fine(current);
-		else
-			current += entered[i];
+		setlocale(LC_ALL, "RU");
+		string expression;
+		cout << "¬ведите выражение: ";
+		getline(cin, expression);
+		cout << "–езультат: " << evaluate(expression) << endl;
 	}
 }
